7f64c17d5816087023086445a6f47a79cec41334
Martinlindblad <36680040+Martinlindblad@users.noreply.github.com>
2021-02-15 15:02:12 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,17 @@ var privateKey = fs.readFileSync('server.key', 'utf8');
 var certificate = fs.readFileSync('server.cert', 'utf8');
 var credentials = { key: privateKey, cert: certificate };
 
-app.use(
-  cors({
-    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-    exposedHeaders: ["authorization"], // you can change the headers
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if( req.method === 'OPTIONS'){
+    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
