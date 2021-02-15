2178c52cab317aefb4a5f99868bd00bb88ff05bc
Martinlindblad <36680040+Martinlindblad@users.noreply.github.com>
2021-02-15 13:50:31 +0100

    cors

--- backend/routes/experience.js
+++ backend/routes/experience.js
@@ -7,16 +7,4 @@ router.route('/').get((req, res) => {
       .catch(error => res.status(400).json('Error: ' + error));
 });
 
-router.route('/add').post((req, res) => {
-  const expSend = req.body.name;
-  const years = req.body.age;
-
-  const newExpValue = new Experience({ expSend, years });
-
-  newExpValue.save()
-      .then(() => res.json('New Exp value added!' + `${newExpValue} ${years} ${expSend}`))
-      .catch(error => res.status(400).json('Error: ' + error));
-});
-
-
 module.exports = router;
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -7,16 +7,6 @@ router.route('/').get((req, res) => {
       .catch(error => res.status(400).json('Error: ' + error));
 });
 
-router.route('/add').post((req, res) => {
-  const profileSend = req.body.name;
-  const years = req.body.age;
-
-  const newProfileValue = new Profile({ profileSend, years });
-
-  newProfileValue.save()
-      .then(() => res.json('New Profile value added!' + `${newProfileValue} ${years} ${profileSend}`))
-      .catch(error => res.status(400).json('Error: ' + error));
-}); 
 
 
 module.exports = router;
